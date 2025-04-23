94d324e0cd0c44ce17cdc776e72afe055b15180e
Monjurul Alam <monjurulalam190629@gmail.com>
2025-04-23 22:32:23 +0600

    productSlice is fully ok

--- src/features/productsSlice.js
+++ src/features/productsSlice.js
@@ -21,5 +21,23 @@ const productsSlice = createSlice({
     name : 'products',
     initialState,
     reducers: {},
+    extraReducers : (builder) => {
+        builder.addCase(fetchProducts.pending, (state) => {
+            state.isLoading = true;
+            state.isError = false;
+        })
+        builder.addCase(fetchProducts.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.products = action.payload;
+        })
+        builder.addCase(fetchProducts.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.error = action.error?.message;
+        })
+    }
     
 })
+
+
+export default productsSlice;
