589ce6264cfa3af9ee2a6afbe885cf9b1cb8b744
Monjurul Alam <monjurulalam190629@gmail.com>
2025-04-23 22:38:43 +0600

    ReduxProvider is added on layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/Component/Navbar/Navbar";
+import { ReduxProvider } from "@/Store/provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,8 +24,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar/>
-        <main className="4">{children}</main>
+        <ReduxProvider>
+          <Navbar />
+          <main className="4">{children}</main>
+        </ReduxProvider>
       </body>
     </html>
   );
