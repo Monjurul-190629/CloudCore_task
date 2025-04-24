6358a72b3a2969e3085f7158a4a4368600f2ae80
Monjurul Alam <monjurulalam190629@gmail.com>
2025-04-24 21:14:06 +0600

    product and order route done

---
+++ src/app/order/page.js
@@ -0,0 +1,12 @@
+import Products from "@/Component/Products/Products";
+
+
+const page = () => {
+    return (
+        <div>
+            <Products />
+        </div>
+    );
+};
+
+export default page;
---
+++ src/app/product/page.js
@@ -0,0 +1,11 @@
+import Products from '@/Component/Products/Products';
+
+const page = () => {
+    return (
+        <div>
+            <Products />
+        </div>
+    );
+};
+
+export default page;
